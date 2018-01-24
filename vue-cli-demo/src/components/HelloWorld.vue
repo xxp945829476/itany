886b0808f86ad5340fea2308517552775a5f6770
徐新鹏 <945829476@qq.com>
2018-01-24 13:16:05 +0800

    vue-cli

---
+++ vue-cli-demo/src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <el-button round>圆形按钮</el-button>
+     <Time></Time>
+  </div>
+
+</template>
+
+<script>
+import Time from './time.vue'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: '下回分解的师傅'
+    }
+  },
+  components:{
+    Time
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+@color:red;
+h1, h2 {
+  font-weight: normal;
+  color: @color
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
